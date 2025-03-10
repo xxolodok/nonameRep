c24347ee3b515b60095aadc81442d0509a7b1d49
xxolodok <matveik157@gmail.com>
2025-03-10 10:22:59 +0600

    Добавление освоблждения памяти для roots после теста

--- quadratic_solver/quadratic_test.c
+++ quadratic_solver/quadratic_test.c
@@ -7,6 +7,7 @@ void quadratic_sover_test() {
   long double eps = 1e-9;
   long double *roots = FindRootsQuadraticSolver(0, 1, 1, eps, 11);
   assert(roots == NULL);
+  free(roots);
 }
 void quadratic_sover_test2() {
   long double eps = 1e-9;
@@ -15,6 +16,7 @@ void quadratic_sover_test2() {
   long double *roots = FindRootsQuadraticSolver(1, 0, -1, eps, 11);
   assert((fabs(roots[0] - test_arg1) < eps) &&
          (fabs(roots[1] - test_arg2) < eps));
+  free(roots);
 }
 void quadratic_sover_test3() {
   long double eps = 1e-9;
@@ -23,11 +25,13 @@ void quadratic_sover_test3() {
   long double *roots = FindRootsQuadraticSolver(1, 0, -0, eps, 11);
   assert((fabs(roots[0] - test_arg1) < eps) &&
          (fabs(roots[1] - test_arg2) < eps));
+  free(roots);
 }
 void quadratic_sover_test4() {
   long double eps = 1e-9;
   long double *roots = FindRootsQuadraticSolver(1, 0, 1, eps, 11);
   assert(roots == NULL); // null функция возвращает когда нет корней
+  free(roots);
 }
 
 void quadratic_sover_test5() {
@@ -37,6 +41,7 @@ void quadratic_sover_test5() {
   long double *roots = FindRootsQuadraticSolver(1, 0, -1e-7, eps, 4);
   assert((fabs(roots[0] - test_arg1) < eps) &&
          (fabs(roots[1] - test_arg2) < eps));
+  free(roots);
 }
 void quadratic_sover_test6() {
   long double eps = 1e-9;
@@ -45,6 +50,7 @@ void quadratic_sover_test6() {
   long double *roots = FindRootsQuadraticSolver(1, -1e+10, -1, eps, 11);
   assert((fabs(roots[0] - test_arg1) < eps) &&
          (fabs(roots[1] - test_arg2) < eps));
+  free(roots);
 }
 void quadratic_sover_test7() {
   long double eps = 1e-7;
@@ -53,6 +59,7 @@ void quadratic_sover_test7() {
   long double *roots = FindRootsQuadraticSolver(1, 0, -1e-8, eps, 7);
   assert((fabs(roots[0] - test_arg1) < eps) &&
          (fabs(roots[1] - test_arg2) < eps));
+  free(roots);
 }
 int main() {
   quadratic_sover_test();
