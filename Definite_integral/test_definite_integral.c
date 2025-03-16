d833cd2c956ab9e7bd070d8d9f1952edbdd8d20d
Leiba-Viktoriya <victuuu0@gmail.com>
2025-03-16 22:02:19 +0600

    Create test_definite_integral.c

---
+++ Definite_integral/test_definite_integral.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include "definite_integral.h"
+
+double example_function(double x) {
+    return -x * x + 9;
+}
+
+double constant_function(double x) {
+    return 5.0;
+}
+
+
+void test_trapezoidalRule() {
+
+    double lower_limit2 = 0.0;
+    double upper_limit2 = 5.0;
+    int num_trapezoids2 = 100;
+    double expected_result2 = 25.0;
+    double tolerance2 = 0.001;
+
+    double actual_result2 = trapezoidalRule(constant_function, lower_limit2, upper_limit2, num_trapezoids2);
+
+    assert(fabs(actual_result2 - expected_result2) < tolerance2);
+    printf("Test passed (Constant function)!\n");
+
+}
+
+int main() {
+    test_trapezoidalRule();
+    return 0;
+}
