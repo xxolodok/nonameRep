1afe153e06fb2e3cd98365a9bed701f5ceeabaee
Leiba-Viktoriya <victuuu0@gmail.com>
2025-03-17 19:52:34 +0600

    Update definite_integral.c

--- Definite_integral/definite_integral.c
+++ Definite_integral/definite_integral.c
@@ -1,47 +1,27 @@
 #include "definite_integral.h"
 #include <stdio.h>
 #include <float.h>
-#include <math.h> 
 
 double trapezoidalRule(Func integrand, double lower_limit, double upper_limit, int num_trapezoids) {
-    double dx;
-    double integral_approximation = 0.0;
-    int num_intervals;
-    int i;
+    double dx = fabs(upper_limit - lower_limit) / num_trapezoids;
     double eps = 1E-7;
+    double integral_approximation = 0.0;
+    double num_intervals;
+    int i;
 
-    if (!isfinite(lower_limit) || !isfinite(upper_limit)) {
-        fprintf(stderr, "Error: Limits of integration must be finite.\n");
+    if (dx <= eps || upper_limit < lower_limit || !isfinite(lower_limit + upper_limit + dx)) {
+        fprintf(stderr, "Error: Invalid input data.\n");
         return NAN;
     }
 
-    if (upper_limit < lower_limit) {
-        fprintf(stderr, "Error: Upper limit must be greater than or equal to lower limit.\n");
-        return NAN;
-    }
-
-    dx = fabs(upper_limit - lower_limit) / num_trapezoids;
-
-    if (num_trapezoids <= 0) {
-        fprintf(stderr, "Error: Number of trapezoids must be positive.\n");
-        return NAN;
-    }
-    
     if (fabs(upper_limit - lower_limit) < eps) {
         return 0.0;
     }
 
-    num_intervals = (int)round(fabs(upper_limit - lower_limit) / dx);
-
-    if (num_intervals <= 0) {
-      fprintf(stderr, "Error: Number of trapezoids is too large, resulting in zero intervals.\n");
-      return NAN;
-    }
-
-
     integral_approximation = (integrand(lower_limit) + integrand(upper_limit)) / 2.0;
+    num_intervals = fabs(upper_limit - lower_limit) / dx;
 
-    for (i = 1; i < num_trapezoids; i++) {
+    for (i = 1; i < num_intervals; i++) {
         integral_approximation += integrand(lower_limit + i * dx);
     }
 
