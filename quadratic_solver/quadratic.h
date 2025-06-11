98c282be835f894f60ec5f46fc8ef363835c2b20
Milwayky <vyryshevaalisa@gmailcom>
2025-06-12 00:53:38 +0600

    Удаление лишней папки task_3.2 из main

--- quadratic_solver/quadratic.h
+++
@@ -1,7 +0,0 @@
-#ifndef QUADRATIC_H
-#define QUADRATIC_H
-
-long double *FindRootsQuadraticSolver(long double a, long double b,
-                                      long double c, long double eps);
-
-#endif
