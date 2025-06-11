ac3cebadf27b0986a786ced09984d1fc57aa9bde
Milwayky <vyryshevaalisa@gmailcom>
2025-06-11 18:32:25 +0600

    Реализация Pool-аллокатора

--- task_2.2/pool_allocator/pool_allocator.h
+++ task_2.2/pool_allocator/pool_allocator.h
@@ -1,19 +1,19 @@
 #ifndef POOL_ALLOCATOR_H
 #define POOL_ALLOCATOR_H
 
-#include <stddef.h>
 #include <assert.h>
+#include <stddef.h>
 
 typedef struct PoolAllocator {
-    size_t block_size;
-    size_t num_blocks;
-    void* memory_pool;
-    void* free_list;
+  size_t block_size;
+  size_t num_blocks;
+  void *memory_pool;
+  void *free_list;
 } PoolAllocator;
 
-int pool_init(PoolAllocator* allocator, size_t block_size, size_t num_blocks);
-void* pool_alloc(PoolAllocator* allocator);
-int pool_free(PoolAllocator* allocator, void* ptr);
-void pool_destroy(PoolAllocator* allocator);
+int pool_init(PoolAllocator *allocator, size_t block_size, size_t num_blocks);
+void *pool_alloc(PoolAllocator *allocator);
+int pool_free(PoolAllocator *allocator, void *ptr);
+void pool_destroy(PoolAllocator *allocator);
 
 #endif
