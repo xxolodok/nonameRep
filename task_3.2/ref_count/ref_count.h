e9bbd2ff3e60f1eae8b651803a61d6b5b6e572fd
Milwayky <vyryshevaalisa@gmailcom>
2025-06-11 23:26:52 +0600

    Сборка мусора

---
+++ task_3.2/ref_count/ref_count.h
@@ -0,0 +1,19 @@
+#ifndef REF_COUNT_H
+#define REF_COUNT_H
+
+#include <stddef.h>
+#include "pool_allocator.h"
+
+typedef struct {
+    size_t count;
+    void *object;
+} ref_count_t;
+
+int ref_count_init(size_t pool_blocks);
+void ref_count_destroy(void);
+
+ref_count_t *ref_count_create(void *object);
+int ref_count_retain(ref_count_t *ref);
+int ref_count_release(ref_count_t *ref);
+
+#endif
